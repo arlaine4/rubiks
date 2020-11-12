4d9f2b867ffb8fc1d6b2ca78276d753668ca3056
MrtnsK <kemartin@student.42.fr>
2020-11-12 17:11:47 +0100

    Added Front move clockwise and counter-clockwise

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import utils
 import cube as c
+import move
 
 if __name__ == "__main__":
 	options = utils.arg_parse_options()
 	cube = c.Cube(options.mix)
+	move.move_F(cube, False)
 	print(options.mix)
