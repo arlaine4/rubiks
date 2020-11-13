6269c67c79eefba5ec7eb747301dc70422512cc5
MrtnsK <kemartin@student.42.fr>
2020-11-13 12:22:34 +0100

    move_R can now do clockwise and counter-clockwise rotation for the face R

--- move.py
+++ move.py
@@ -84,24 +84,24 @@ def move_R(c, clockwise):
         c.cube[0][1][2] = tmp[1]
         c.cube[0][2][2] = tmp[2]
     else:
-        c.cube[0] = np.rot90(c.cube[0], k=1, axes=(0, 1))
-        tmp = deepcopy(c.cube[1])
-        # D to R
-        c.cube[1][2][0] = c.cube[5][0][0] 
-        c.cube[1][1][0] = c.cube[5][0][1] 
-        c.cube[1][0][0] = c.cube[5][0][2] 
-        # L to D
-        c.cube[5][0][0] = c.cube[4][0][2]
-        c.cube[5][0][1] = c.cube[4][1][2]
-        c.cube[5][0][2] = c.cube[4][2][2]
-        # U to L
-        c.cube[4][2][2] = c.cube[2][2][0] 
-        c.cube[4][1][2] = c.cube[2][2][1] 
-        c.cube[4][0][2] = c.cube[2][2][2] 
-        # R to U
-        c.cube[2][2][0] = tmp[0][0]
-        c.cube[2][2][1] = tmp[1][0]
-        c.cube[2][2][2] = tmp[2][0]
+        c.cube[1] = np.rot90(c.cube[1], k=1, axes=(0, 1))
+        tmp = deepcopy(c.cube[5][2])
+        # F to D
+        c.cube[5][2][2] = c.cube[0][2][2] 
+        c.cube[5][1][2] = c.cube[0][2][1] 
+        c.cube[5][0][2] = c.cube[0][2][0] 
+        # U to F
+        c.cube[0][2][2] = c.cube[2][2][2]
+        c.cube[0][1][2] = c.cube[2][1][2]
+        c.cube[0][0][2] = c.cube[2][0][2]
+        # B to U
+        c.cube[2][2][2] = c.cube[3][0][0]  
+        c.cube[2][1][2] = c.cube[3][1][0]  
+        c.cube[2][0][2] = c.cube[3][2][0]  
+        # D to B
+        c.cube[3][2][0] = tmp[0]
+        c.cube[3][1][0] = tmp[1]
+        c.cube[3][0][0] = tmp[2]
 
     print("AFTER MOVE_R")
     print_cube(c)
