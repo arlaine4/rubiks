497e48bdb9c1c16829a21564f313456f4e06dd37
MrtnsK <kemartin@student.42.fr>
2020-11-16 16:35:45 +0100

    added move_B that move the Back of the rubiks cube clockwise and counter-clockwise

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import move
 if __name__ == "__main__":
 	options = utils.arg_parse_options()
 	cube = c.Cube(options.mix)
-	move.move_R(cube, True)
+	move.move_B(cube, True)
 	print(options.mix)
--- move.py
+++ move.py
@@ -35,6 +35,7 @@ def move_F(c, clockwise):
         c.cube[4][0][2] = tmp[0]
         c.cube[4][1][2] = tmp[1]
         c.cube[4][2][2] = tmp[2]
+    # F rotate counter-clockwise
     else:
         c.cube[0] = np.rot90(c.cube[0], k=1, axes=(0, 1))
         tmp = deepcopy(c.cube[1])
@@ -58,12 +59,10 @@ def move_F(c, clockwise):
     print("AFTER MOVE_F")
     print_cube(c)
 
-# if the order is F R U B L D
-#                 0 1 2 3 4 5
 def move_R(c, clockwise):
     c.cube[1][0][0] = 9
     print_cube(c)
-    # F rotate clockwise
+    # R rotate clockwise
     if clockwise:
         c.cube[1] = np.rot90(c.cube[1], k=1, axes=(1, 0))
         tmp = deepcopy(c.cube[5][2])
@@ -83,6 +82,7 @@ def move_R(c, clockwise):
         c.cube[0][0][2] = tmp[0]
         c.cube[0][1][2] = tmp[1]
         c.cube[0][2][2] = tmp[2]
+    # R rotate counter-clockwise
     else:
         c.cube[1] = np.rot90(c.cube[1], k=1, axes=(0, 1))
         tmp = deepcopy(c.cube[5][2])
@@ -104,4 +104,51 @@ def move_R(c, clockwise):
         c.cube[3][0][0] = tmp[2]
 
     print("AFTER MOVE_R")
+    print_cube(c)
+
+def move_B(c, clockwise):
+    c.cube[3][0][0] = 9
+    print_cube(c)
+    # B rotate clockwise
+    if clockwise:
+        c.cube[3] = np.rot90(c.cube[3], k=1, axes=(1, 0))
+        tmp = deepcopy(c.cube[5][0])
+        # L to D
+        c.cube[5][2][2] = c.cube[4][0][0]
+        c.cube[5][2][1] = c.cube[4][1][0]
+        c.cube[5][2][0] = c.cube[4][2][0]
+        # U to L
+        c.cube[4][0][0] = c.cube[2][2][0]
+        c.cube[4][1][0] = c.cube[2][2][1]
+        c.cube[4][2][0] = c.cube[2][2][2]
+        # R to U
+        c.cube[2][0][2] = c.cube[1][0][2]
+        c.cube[2][0][1] = c.cube[1][1][2]
+        c.cube[2][0][0] = c.cube[1][2][2]
+        # D to R
+        c.cube[1][0][2] = tmp[0]
+        c.cube[1][1][2] = tmp[1]
+        c.cube[1][2][2] = tmp[2]
+    # B rotate counter-clockwise
+    else:
+        c.cube[3] = np.rot90(c.cube[3], k=1, axes=(0, 1))
+        tmp = deepcopy(c.cube[4])
+        # D to L
+        c.cube[4][2][0] = c.cube[5][0][0] 
+        c.cube[4][1][0] = c.cube[5][0][1] 
+        c.cube[4][0][0] = c.cube[5][0][2] 
+        # R to D
+        c.cube[5][2][0] = c.cube[1][0][2]
+        c.cube[5][2][1] = c.cube[1][1][2]
+        c.cube[5][2][2] = c.cube[1][2][2]
+        # U to R
+        c.cube[1][2][2] = c.cube[2][2][0] 
+        c.cube[1][1][2] = c.cube[2][2][1] 
+        c.cube[1][0][2] = c.cube[2][2][2] 
+        # L to U
+        c.cube[2][0][0] = tmp[0][0]
+        c.cube[2][0][1] = tmp[1][0]
+        c.cube[2][0][2] = tmp[2][0]
+
+    print("AFTER MOVE_B")
     print_cube(c)
