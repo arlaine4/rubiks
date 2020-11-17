3c805b81c5928d3aa4bfb5f712584cd3e1473a9e
tuturr <arthur.laine99@gmail.com>
2020-11-17 15:50:26 +0100

    bug fix and some error handling

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import argparse
 import cube
 import move
+import sys
 
 def arg_parse_options():
 	parser = argparse.ArgumentParser()
@@ -10,13 +11,29 @@ def arg_parse_options():
 	return options
 
 def	select_move_function_to_call(move_id, cube):
-	if move_id == "F":
-		cube = move.move_F(cube, False) if "'" in move_id else move.move_F(cube, True)
-	elif move_id == "R":
-		cube = move.move_R(cube, False) if "'" in move_id else move.move_R(cube, True)
-	elif move_id == "B":
-		cube = move.move_B(cube, False) if "'" in move_id else move.move_B(cube, True)
-	return cube
+	repeat = 1
+	nb_letter = 0
+	for c in move_id:
+		if c.isdigit():
+			repeat = int(c)
+		elif c.isalpha():
+			nb_letter += 1
+		if nb_letter > 1:
+			print("Error in mix parameter, please enter a valid mix.")
+			sys.exit(0)
+	if nb_letter == 0:
+		print("Error in mix parameter, please_enter a valid mix.")
+		sys.exit(0)
+	if "F" in move_id:
+		for loop in range(repeat):
+			cube = move.move_F(cube, False) if "'" in move_id else move.move_F(cube, True)
+	elif "R" in move_id:
+		for loop in range(repeat):
+			cube = move.move_R(cube, False) if "'" in move_id else move.move_R(cube, True)
+	elif "B" in move_id:
+		for loop in range(repeat):
+			cube = move.move_B(cube, False) if "'" in move_id else move.move_B(cube, True)
+	return cube.cube
 
 def	shuffle_cube(mix, c):
 	c.main_walk = mix
