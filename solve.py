835f8253385b49fb696b56239d1321f611b68da9
tuturr <arthur.laine99@gmail.com>
2020-11-25 18:00:14 +0100

    colored terminal visual

--- solve.py
+++ solve.py
@@ -2,11 +2,14 @@ import utils
 import cube as c
 import move
 import STEPS.step_one as s
+import time
 
 def	process_steps(cube):
 	c.print_cube(cube)
 	cube = s.step_one(cube)
 	print("After solve step_one :")
-	c.print_cube(cube)
+	for i in range(100):
+		c.print_cube(cube)
+		time.sleep(1)
 	print(s.check_valid_step_one(cube))
 	return cube
