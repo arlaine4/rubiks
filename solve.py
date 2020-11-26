3739523076a94f27f1da6c265f79ef8af9b3ff6b
MrtnsK <kemartin@student.42.fr>
2020-11-26 16:19:35 +0100

    starting twophase algo, need to decript line 18-19 and 28

--- solve.py
+++ solve.py
@@ -1,15 +1,10 @@
 import utils
 import cube as c
 import move
-import STEPS.step_one as s
 import time
+import twophase as tp
 
 def	process_steps(cube):
 	c.print_cube(cube)
-	cube = s.step_one(cube)
-	print("After solve step_one :")
-	for i in range(100):
-		c.print_cube(cube)
-		time.sleep(1)
-	print(s.check_valid_step_one(cube))
+	print(tp.phase_one(cube))
 	return cube
---
+++ twophase.py
@@ -0,0 +1,31 @@
+# Source :
+# http://kociemba.org/math/twophase.htm
+# https://github.com/Minipeps/TwoPhase-Cuber
+
+# Array "edges" to know wich edges are on his home position
+# 0  1  2  3  4  5  6  7  8  9  10 11
+# UR UF UL UB DR DF DL DB FR FL BL BR
+
+def convert_cube_to_string(c):
+    return None
+
+def phase_one(c):
+    # s_cube = convert_cube_to_string(c)
+    # edges = [False, False, False, False, False, False, False, False, False, False, False, False]
+    # current_edge = 0
+    # k = 0 # used for binomial coefficient
+    # n = 0 # used for binomial coefficient
+    # for i in range(12): # Position des differents edge
+    #     # if PEdge^[ed].e >= FR
+    #         edges[i] = True
+    # s = None
+    # k = 3
+    # n = 11
+    # while k >= 0:
+    #     if occupied[n]:
+    #         k -= 1
+    #     else:
+    #         s = s + C(n,k)
+    #     n -= 1
+    # result = s
+    return c
