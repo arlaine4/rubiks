35246477901b8b06e27c2680b90ba010b0a15459
tuturr <arthur.laine99@gmail.com>
2020-11-30 21:30:06 +0100

    linked main with visual, finally got how to add colors, still really buggy and missing some surfaces, but the idea is there

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import utils
 import cube as c
 import move
 import solve
+import visual as visu
 
 if __name__ == "__main__":
 	options = utils.arg_parse_options()
 	cube = c.Cube(options.mix)
-	utils.shuffle_cube(options.mix, cube)
-	cube = solve.process_steps(cube)
+	if options.visu:
+		visu.main_visual(cube, options.mix)
+	else:
+		utils.shuffle_cube(options.mix, cube)
+		cube = solve.process_steps(cube)
 	print(options.mix)
