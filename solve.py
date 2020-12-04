35fce89c8e90101fcc14a030e28777dd181a7d55
Kevin <kemartin@student.42.fr>
2020-12-04 12:04:32 +0100

    little modification to test the convert twophase

--- solve.py
+++ solve.py
@@ -3,8 +3,10 @@ import cube as c
 import move
 import time
 import twophase as tp
+import twophase_convert as tpc
 
 def	process_steps(cube):
 	c.print_cube(cube)
-	print(tp.phase_one(cube))
+	tpc.convert(cube)
+	# print(tp.phase_one(cube))
 	return cube
