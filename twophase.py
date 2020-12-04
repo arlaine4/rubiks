069f5003e7316a4c02983c9cdf0127e825ef1ba1
Kevin <kemartin@student.42.fr>
2020-12-04 16:41:10 +0100

    binomial coef function, didn't test it atm

--- twophase.py
+++ twophase.py
@@ -1,6 +1,20 @@
 import twophase_enum as tpe
 import twophase_convert as tpc
 
+def c(n, k):
+    """Binomial coefficient [n choose k]."""
+    if n < k:
+        return 0
+    if k > n // 2:
+        k = n - k
+    s, i, j = 1, n, 1
+    while i != n - k:
+        s *= i
+        s //= j
+        i -= 1
+        j += 1
+    return s
+	
 def phase_one(c):
 	tpc.convert(c)
 	return c
