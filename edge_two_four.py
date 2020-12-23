948c8cb9a7970ef1a32391fd036fd8b13b187bf5
tuturr <arthur.laine99@gmail.com>
2020-12-23 15:00:01 +0100

    rework nb bad edges in hta, clean prints, generic cases for edge orientation four is good for F and B

--- edge_two_four.py
+++ edge_two_four.py
@@ -42,15 +42,15 @@ def	edge_o_four(cube, pos):
 			# 				bad edge sur right
 			if not [0, 1, 2] in pos or [1, 1, 0] in pos:
 				print("FR is clear")
-				if (p[0] == 1 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 1 and p[2] == 2)
+				if (p[0] == 1 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 1 and p[2] == 2):
 					m.move_R(cube, True)
 					lst_move.append("R")
 					print("R move")
-				if (p[0] == 1 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[2] == 2)
+				if (p[0] == 1 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[2] == 2):
 					m.move_R(cube, False)
 					lst_move.append("R'")
 					print("R' move")
-				if (p[0] == 1 and p[1] == 1 and p[2] == 2) or (p[0] == 3 and p[1] == 1 and p[2] == 0)
+				if (p[0] == 1 and p[1] == 1 and p[2] == 2) or (p[0] == 3 and p[1] == 1 and p[2] == 0):
 					m.move_R(cube, True)
 					m.move_R(cube, True)
 					lst_move.append("R2")
@@ -62,15 +62,15 @@ def	edge_o_four(cube, pos):
 			# 				bad edge sur up
 			if not [2, 2, 1] in pos or [0, 0, 1] in pos:
 				print("UF is clear")
-				if (p[0] == 2 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 0 and p[2] == 1)
+				if (p[0] == 2 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 0 and p[2] == 1):
 					m.move_U(cube, True)
 					lst_move.append("U")
 					print("U")
-				if (p[0] == 2 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 0 and p[1] == 1)
+				if (p[0] == 2 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 0 and p[1] == 1):
 					m.move_U(cube, False)
 					lst_move.append("U'")
 					print("U'")
-				if (p[0] == 2 and p[1] == 0 and p[2] == 1) or (p[0] == 3 and p[1] == 0 and p[2] == 1)
+				if (p[0] == 2 and p[1] == 0 and p[2] == 1) or (p[0] == 3 and p[1] == 0 and p[2] == 1):
 					m.move_U(cube, True)
 					m.move_U(cube, True)
 					lst_move.append("U2")
@@ -82,25 +82,102 @@ def	edge_o_four(cube, pos):
 			# 				bad edge sur down
 			if [5, 0, 1] in pos or [0, 2, 1] in pos:
 				print("DF is clear")
-				if (p[0] == 5 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 2 and p[2] == 1)
+				if (p[0] == 5 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 2 and p[2] == 1):
 					m.move_D(cube, True)
 					lst_move.append("D")
 					print("D")
-				if (p[0] == 5 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 2 and p[2] == 1)
+				if (p[0] == 5 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 2 and p[2] == 1):
 					m.move_D(cube, False)
 					lst_move.append("D'")
 					print("D'")
-				if (p[0] == 5 and p[1] == 2 and p[2] == 1) or (p[0] == 3 and p[1] == 2 and p[2] == 1)
+				if (p[0] == 5 and p[1] == 2 and p[2] == 1) or (p[0] == 3 and p[1] == 2 and p[2] == 1):
 					m.move_D(cube, True)
 					m.move_D(cube, True)
 					lst_move.append("D2")
 					print("D2")
 			#
 			#--------------------------------------------------#
-		
+	
+		if fb_prio == "B":
 			
-		# if fb_prio == "B":
-		# select_move_lst, nb_moove = utils.select_best_move_f_b(cube, p)
-		# lst_move.append(select_move_lst) ; nb_mooves.append(nb_moove)
+			#--------------------------------------------------#
+			# 				bad edge sur left
+			if not [3,1,2] in pos or not [4,1,0] in pos:
+				print("FL is clear")
+				if (p[0] == 5 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 2 and p[2] == 1):
+					m.move_L(cube, True)
+					lst_move.append("L")
+					print("L move")
+				if (p[0] == 4 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[2] == 0):
+					m.move_L(cube, False)
+					lst_move.append("L'")
+					print("L' move")
+				if (p[0] == 4 and p[1] == 1 and p[2] == 2) or (p[0] == 0 and p[1] == 1 and p[2] == 0):
+					m.move_L(cube, True)
+					m.move_L(cube, True)
+					lst_move.append("L2")
+					print("L2 move")
+			#
+			#--------------------------------------------------#
+
+			#--------------------------------------------------#
+			# 				bad edge sur right
+			if not [1, 1, 2] in pos or [3, 1, 0] in pos:
+				print("FR is clear")
+				if (p[0] == 2 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 0 and p[2] == 1):
+					m.move_R(cube, True)
+					lst_move.append("R")
+					print("R move")
+				if (p[0] == 1 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 1 and p[2] == 2):
+					m.move_R(cube, False)
+					lst_move.append("R'")
+					print("R' move")
+				if (p[0] == 0 and p[1] == 1 and p[2] == 2) or (p[0] == 1 and p[1] == 1 and p[2] == 0):
+					m.move_R(cube, True)
+					m.move_R(cube, True)
+					lst_move.append("R2")
+					print("R2 move")
+			#
+			#--------------------------------------------------#
+
+			#--------------------------------------------------#
+			# 				bad edge sur up
+			if not [2, 0, 1] in pos or [3, 0, 1] in pos:
+				print("UF is clear")
+				if (p[0] == 4 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[2] == 0):
+					m.move_U(cube, True)
+					lst_move.append("U")
+					print("U")
+				if (p[0] == 1 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[1] == 2):
+					m.move_U(cube, False)
+					lst_move.append("U'")
+					print("U'")
+				if (p[0] == 2 and p[1] == 2 and p[2] == 1) or (p[0] == 0 and p[1] == 0 and p[2] == 1):
+					m.move_U(cube, True)
+					m.move_U(cube, True)
+					lst_move.append("U2")
+					print("U2")
+			#
+			#--------------------------------------------------#
+	
+			#--------------------------------------------------#
+			# 				bad edge sur down
+			if [5, 2, 1] in pos or [3, 2, 1] in pos:
+				print("DF is clear")
+				if (p[0] == 1 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 1 and p[2] == 2):
+					m.move_D(cube, True)
+					lst_move.append("D")
+					print("D")
+				if (p[0] == 5 and p[1] == 1 and p[2] == 0) or (p[0] == 4 and p[1] == 2 and p[2] == 1):
+					m.move_D(cube, False)
+					lst_move.append("D'")
+					print("D'")
+				if (p[0] == 0 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 0 and p[2] == 1):
+					m.move_D(cube, True)
+					m.move_D(cube, True)
+					lst_move.append("D2")
+					print("D2")
+	m.move_F(cube, True) if fb_prio == "F" else m.move_B(cube, True)
+	lst_move.append("F") if fb_prio == "F" else lst_move.append("B")
 	c.print_cube(cube)
 	return cube
--- hta.py
+++ hta.py
@@ -21,144 +21,104 @@ def	step_one(cube):
 def	edge_orientation(cube):
 	#Pour edge orientation on vas first compter le nb
 	#de edge qui sont mal orientees
-	nb_bad_edges, pos_bad_edges = edge_o_detection(cube)
+	pos_bad_edges = edge_o_detection(cube)
 	c.print_cube(cube)
-	print("Number of bad edges : {}".format(nb_bad_edges))
+	print("Number of bad edges \033[33mbefore\033[0m : {}".format(len(pos_bad_edges)))
 	for elem in pos_bad_edges:
 		print(elem)
-	cube = edge_orientation_strategy(cube, nb_bad_edges, pos_bad_edges)
+	cube = edge_orientation_strategy(cube, pos_bad_edges)
+	pos_bad_edges = edge_o_detection(cube)
+	c.print_cube(cube)
+	print("Number of bad edges \033[35mafter\033[0m: {}".format(len(pos_bad_edges)))
 	return cube
 
-def	edge_orientation_strategy(cube, nb_bad_e, pos):
+def	edge_orientation_strategy(cube, pos):
+	nb_bad_e = len(pos)
 	if nb_bad_e == 2: #F R U F R2
 		cube = edge_t_f.edge_o_two(cube, pos)
 	elif nb_bad_e == 4: #F R U R B L2 B'  D R F
 		cube = edge_t_f.edge_o_four(cube, pos)
-		pass
 	return cube		
-
-"""def	edge_o_two(cube, pos):
-	lst_move = []
-	nb_mooves = []
-	#for p in pos:
-		#select_move_lst, nb_moove = utils.select_best_move_f_b(cube, p)
-		#lst_move.append(select_move_lst) ; nb_mooves.append(nb_moove)
-	return cube
-
-def	edge_o_four(cube, pos):
-	lst_move = []
-	nb_mooves = []
-	fb_prio = utils.front_or_back(pos)
-	print(fb_prio)
-	for p in pos:
-		if fb_prio == "F":
-			if not [0,1,0] in pos or not [4,1,2] in pos:
-				print("FL is clear")
-				if (p[0] == 4 and p[1] == 0 and p[2] == 1) or (p[0] == 2 and p[1] == 1 and p[2] == 0):
-					m.move_L(cube, True)
-					lst_move.append("L")
-					print("L move")
-				if (p[0] == 4 and p[1] == 2 and p[2] == 1) or (p[0] == 5 and p[1] == 1 and p[2] == 0):
-					m.move_L(cube, False)
-					lst_move.append("L'")
-					print("L' move")
-				if (p[0] == 4 and p[1] == 1 and p[2] == 0) or (p[0] == 3 and p[1] == 1 and p[2] == 2):
-					m.move_L(cube, True)
-					m.move_L(cube, True)
-					lst_move.append("L2")
-					print("L2 move")
-		# if fb_prio == "B":
-		# select_move_lst, nb_moove = utils.select_best_move_f_b(cube, p)
-		# lst_move.append(select_move_lst) ; nb_mooves.append(nb_moove)
-	c.print_cube(cube)
-	return cube"""
 
 def	edge_o_detection(cube):
 	#calcul en deux etapes du nb de edges mal orientees
-	bad_edges_u_d = 0
-	bad_edges_f_b = 0
 	pos_u_d = []
 	pos_f_b = []
 	pos = []
-	bad_edges_u_d, pos_u_d = o_detection_u_d(cube)
-	bad_edges_f_b, pos_f_b = o_detection_f_b(cube)
+	pos_u_d = o_detection_u_d(cube)
+	pos_f_b = o_detection_f_b(cube)
 	pos = pos_u_d + pos_f_b
-	return (bad_edges_u_d + bad_edges_f_b), pos 
+	return pos
 
 def	o_detection_u_d(c):
 	#detection Up et Down faces
-	bad = 0
 	pos = []
 	if c.cube[2][0][1] == 4 or c.cube[2][0][1] == 1:
-		bad += 1 ; pos.append([2, 0, 1])
+		pos.append([2, 0, 1])
 	if c.cube[2][1][0] == 4 or c.cube[2][1][0] == 1:
-		bad += 1 ; pos.append([2, 1, 0])
+		pos.append([2, 1, 0])
 	if c.cube[2][1][2] == 4 or c.cube[2][1][2] == 1:
-		bad += 1 ; pos.append([2, 1, 2])
+		pos.append([2, 1, 2])
 	if c.cube[2][2][1] == 4 or c.cube[2][2][1] == 1:
-		bad += 1 ; pos.append([2, 2, 1])
+		pos.append([2, 2, 1])
 	if c.cube[2][0][1] == 0 or c.cube[2][0][1] == 3:
 		if c.cube[3][0][1] == 2 or c.cube[3][0][1] == 5:
-			bad += 1 ; pos.append([3, 0, 1])
+			pos.append([3, 0, 1])
 	if c.cube[2][1][0] == 0 or c.cube[2][1][0] == 3:
 		if c.cube[4][0][1] == 2 or c.cube[4][0][1] == 5:
-			bad += 1 ; pos.append([4, 0, 1])
+			pos.append([4, 0, 1])
 	if c.cube[2][1][2] == 0 or c.cube[2][1][2] == 3:
 		if c.cube[1][0][1] == 2 or c.cube[1][0][1] == 5:
-			bad += 1 ; pos.append([1, 0, 1])
+			pos.append([1, 0, 1])
 	if c.cube[2][2][1] == 0 or c.cube[2][2][1] == 3:
 		if c.cube[0][0][1] == 2 or c.cube[0][0][1] == 5:
-			bad += 1 ; pos.append([0, 0, 1])
+			pos.append([0, 0, 1])
 	# Down
 	if c.cube[5][0][1] == 4 or c.cube[5][0][1] == 1:
-		bad += 1 ; pos.append([5, 0, 1])
+		pos.append([5, 0, 1])
 	if c.cube[5][1][0] == 4 or c.cube[5][1][0] == 1:
-		bad += 1 ; pos.append([5, 1, 0])
+		pos.append([5, 1, 0])
 	if c.cube[5][1][2] == 4 or c.cube[5][1][2] == 1:
-		bad += 1 ; pos.append([5, 1, 2])
+		pos.append([5, 1, 2])
 	if c.cube[5][2][1] == 4 or c.cube[5][2][1] == 1:
-		bad += 1 ; pos.append([5, 2, 1])
+		pos.append([5, 2, 1])
 	if c.cube[5][0][1] == 0 or c.cube[5][0][1] == 3:
 		if c.cube[0][2][1] == 2 or c.cube[0][2][1] == 5:
-			bad += 1 ; pos.append([0, 2, 1])
+			pos.append([0, 2, 1])
 	if c.cube[5][1][0] == 0 or c.cube[5][1][0] == 3:
 		if c.cube[4][2][1] == 2 or c.cube[4][2][1] == 5:
-			bad += 1 ; pos.append([4, 2, 1])
+			pos.append([4, 2, 1])
 	if c.cube[5][1][2] == 0 or c.cube[5][1][2] == 3:
 		if c.cube[1][2][1] == 2 or c.cube[1][2][1] == 5:
-			bad += 1 ; pos.append([1, 2, 1])
+			pos.append([1, 2, 1])
 	if c.cube[5][2][1] == 0 or c.cube[5][2][1] == 3:
 		if c.cube[3][2][1] == 2 or c.cube[3][2][1] == 5:
-			bad += 1 ; pos.append([5, 2, 1])
-	#print("Bad after U_D : ", bad)
-	return bad, pos
+			pos.append([5, 2, 1])
+	return pos
 
 def	o_detection_f_b(c):
 	#detection Front et Back faces
-	bad = 0
 	pos = []
 	# Front
 	if c.cube[0][1][0] == 4 or c.cube[0][1][0] == 1:
-		bad += 1 ; pos.append([0, 1, 0])
+		pos.append([0, 1, 0])
 	if c.cube[0][1][2] == 4 or c.cube[0][1][2] == 1:
-		bad += 1 ; pos.append([0, 1, 2])
+		pos.append([0, 1, 2])
 	if c.cube[0][1][0] == 0 or c.cube[0][1][0] == 3:
 		if c.cube[4][1][2] == 5 or c.cube[4][1][2] == 2:
-			bad += 1 ; pos.append([4, 1, 2])
+			pos.append([4, 1, 2])
 	if c.cube[0][1][2] == 0 or c.cube[0][1][2] == 3:
 		if c.cube[1][1][0] == 2 or c.cube[1][1][0] == 5:
-			bad += 1 ; pos.append([1, 1, 0])
+			pos.append([1, 1, 0])
 	# Back
 	if c.cube[3][1][0] == 4 or c.cube[3][1][0] == 1:
-		bad += 1 ; pos.append([3, 1, 0])
+		pos.append([3, 1, 0])
 	if c.cube[3][1][2] == 4 or c.cube[3][1][2] == 1:
-		bad += 1 ; pos.append([3, 1, 2])
+		pos.append([3, 1, 2])
 	if c.cube[3][1][0] == 0 or c.cube[3][1][0] == 3:
 		if c.cube[1][1][2] == 5 or c.cube[1][1][2] == 2:
-			bad += 1 ; pos.append([1, 1, 2])
+			pos.append([1, 1, 2])
 	if c.cube[3][1][2] == 0 or c.cube[3][1][2] == 3:
 		if c.cube[4][1][0] == 2 or c.cube[4][1][0] == 5:
-			bad += 1 ; pos.append([4, 1, 0])
-	#print("Bad after F_B : ", bad)
-	return bad, pos
-#----------------------------------------------------#
+			pos.append([4, 1, 0])
+	return pos
