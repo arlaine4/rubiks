744f1bd07e8aee9c03a6aa98a2ba92f3b0382805
tuturr <arthur.laine99@gmail.com>
2021-01-07 19:41:45 +0100

    bug fix but good_edges_up contain bad values

--- edge_placement.py
+++ edge_placement.py
@@ -4,27 +4,34 @@ import visual as visu
 import utils
 import hta
 
-def     edges_placement(cube, bad_edges, ggood_edges_up, good_edges_down, lst_moves):
-        t_p_u = [[2, 0, 1, 3, 0, 1], [2, 1, 0, 4, 0, 1], [2, 2, 1, 0, 0, 1], [2, 1, 2, 1, 0, 1]]
-        t_p_d = [[5, 0, 1, 0, 2, 1], [5, 1, 0, 4, 2, 1], [5, 2, 1, 3, 2, 1], [5, 1, 2, 1, 2, 1]]
-        value_pos_up = [[2, 3], [2, 4], [2, 0], [2, 1]]
-        value_pos_down = [[5, 0], [5, 4], [5, 3], [5, 1]]
-        lst_order_edges = []
-        lst_order_values = []
-        id_current = 0
-        #separer good edge en up et down
-        #-----------------------------------------------------------#
-        #                       Partie Up                           #
-
-        #--------------------------#
-        # Selection starting point #
-        if len(good_edges) > 0:
-            lst_order_edges.append(good_edges[0])
-            lst_order_values.append([good_edges[0][0], cube.cube[good_edges[0]][good_edges[1]][good_edges[2]])
-            #check les good_edges suivantes si elles sont deja sur la bonne face et dans le bon ordre
-            #meme si elle sont pas a la bonne place
-            #si elle sont bonnes, append a order_edge et order_values
-        elif len(good_edges) == 0:
+def		edges_placement(cube, bad_edges, good_edges_up, good_edges_down, lst_moves):
+		t_p_u = [[2, 0, 1, 3, 0, 1], [2, 1, 0, 4, 0, 1], [2, 2, 1, 0, 0, 1], [2, 1, 2, 1, 0, 1]]
+		t_p_d = [[5, 0, 1, 0, 2, 1], [5, 1, 0, 4, 2, 1], [5, 2, 1, 3, 2, 1], [5, 1, 2, 1, 2, 1]]
+		value_pos_up = [[2, 3], [2, 4], [2, 0], [2, 1]]
+		value_pos_down = [[5, 0], [5, 4], [5, 3], [5, 1]]
+		lst_order_edges = []
+		lst_order_values = []
+		id_current = 0
+		#separer good edge en up et down
+		#-----------------------------------------------------------#
+		#                       Partie Up                           #
+		#--------------------------#
+		# Selection starting point #
+		if len(good_edges_up) > 0:
+			lst_order_edges.append(good_edges_up[0])
+			lst_order_values.append([good_edges_up[0][0], cube.cube[good_edges_up[0][0]][good_edges_up[0][1]][good_edges_up[0][2]]])
+		elif len(good_edges_up) == 0:
+			pass
+		print("order edges : ", lst_order_edges, "order values : ", lst_order_values)
+		return cube, lst_moves
+		"""if len(good_edges) > 0:
+			lst_order_edges.append(good_edges[0])
+			lst_order_values.append([good_edges[0][0], cube.cube[good_edges[0]][good_edges[1]][good_edges[2]]])
+			#check les good_edges suivantes si elles sont deja sur la bonne face et dans le bon ordre
+			#meme si elle sont pas a la bonne place
+			#si elle sont bonnes, append a order_edge et order_values
+ 		elif len(good_edges) == 0:
+			pass
             #chercher edge en ud_slice, prendre la premiere que on trouve
                 #move un edge ud_slice sur la face up sur l'edge la plus proche, qui coute le moins de coups
             ##chercher une edge de up qui serait sur down
@@ -32,16 +39,16 @@ def     edges_placement(cube, bad_edges, ggood_edges_up, good_edges_down, lst_mo
             #re assigner lst_order_edges et lst_order_values
         #-------------------------#
 
-        #deplacer une a une les edge a la suite de lst_order[i] en checkant lst_order_values[i] pour savoir
-        #de quel edge de la face on part
+		#deplacer une a une les edge a la suite de lst_order[i] en checkant lst_order_values[i] pour savoir
+		#de quel edge de la face on part
 
-        #on passe pas a l'edge suivante tant que la courrante n'est pas a la suite de la precedente
+		#on passe pas a l'edge suivante tant que la courrante n'est pas a la suite de la precedente
 
-        #forbid F and B moves
-        #append every move to lst_moves
-        #ne pas faire de move de la face ou on se situe si ca nique l'ordre ni qui niquerai le placement
-        #d'une edge deja dans lst_order_edges
-        return cube, lst_moves
+		#forbid F and B moves
+		#append every move to lst_moves
+		#ne pas faire de move de la face ou on se situe si ca nique l'ordre ni qui niquerai le placement
+		#d'une edge deja dans lst_order_edges
+		return cube, lst_moves"""
 
 
 """def     edges_placement(cube, bad_edges, good_edges, lst_moves):
