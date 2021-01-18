944ee79fb067f325992fe46e178ecc83ee4edffa
arthur laine <arthur.laine99@gmail.com>
2021-01-18 11:58:42 +0100

    fixed moves issues, divided move functions into two separeted functions, move and move_counter, code should be ready for algorithm implementation

--- v2/SecondaryFunctions/mix.py
+++ v2/SecondaryFunctions/mix.py
@@ -13,39 +13,39 @@ class Mix():
     def runMix(self, lst_moves, cube):
         for move in lst_moves:
             if move == "F":
-                cube.move_front(False)
+                cube.move_front()
             elif move == "F'":
-                cube.move_front(True)
+                cube.move_front_counter()
             elif move == "F2":
                 cube.move_f2()
             elif move == "B":
-                cube.move_back(False)
+                cube.move_back()
             elif move == "B'":
-                cube.move_back(True)
+                cube.move_back_counter()
             elif move == "B2":
                 cube.move_b2()
             elif move == "D":
-                cube.move_down(False)
+                cube.move_down()
             elif move == "D'":
-                cube.move_down(True)
+                cube.move_down_counter()
             elif move == "D2":
                 cube.move_d2()
             elif move == "L":
-                cube.move_left(False)
+                cube.move_left()
             elif move == "L'":
-                cube.move_left(True)
+                cube.move_left_counter()
             elif move == "L2":
                 cube.move_l2()
             elif move == "R":
-                cube.move_right(False)
+                cube.move_right()
             elif move == "R'":
-                cube.move_right(True)
+                cube.move_right_counter()
             elif move == "R2":
                 cube.move_r2()
             elif move == "U":
-                cube.move_up(False)
+                cube.move_up()
             elif move == "U'":
-                cube.move_up(True)
+                cube.move_up_counter()
             elif move == "U2":
                 cube.move_u2()
         return cube
--- v2/cubik.py
+++ v2/cubik.py
@@ -22,112 +22,119 @@ class Cube():
             lst.append(s)
         return lst
 
-    def move_up(self, counter_clockwise):
-        tmp_row = self.right[0]
-        if not counter_clockwise:
-            self.right[0] = self.back[0]
-            self.back[0] = self.left[0]
-            self.left[0] = self.front[0]
-            self.front[0] = tmp_row
-            self.moveFront(self.up, 'u')
-        elif counter_clockwise:
-            self.right[0] = self.front[0]
-            self.front[0] = self.left[0]
-            self.left[0] = self.back[0]
-            self.back[0] = tmp_row
-            self.moveFrontCounter(self.up, 'u')
+    def move_up(self):
+        tmp = self.right[0]
+        self.right[0] = self.back[0]
+        self.back[0] = self.left[0]
+        self.left[0] = self.front[0]
+        self.front[0] = tmp
+        self.moveFront(self.up, 'u')
 
-    def move_down(self, counter_clockwise):
-        i = self.size - 1
-        tmp_row = self.right[i]
-        if not counter_clockwise:
-            self.right[i] = self.front[i]
-            self.front[i] = self.left[i]
-            self.left[i] = self.back[i]
-            self.back[i] = tmp_row
-            self.moveFront(self.down, 'd')
-        elif counter_clockwise:
-            self.right[i] = self.back[i]
-            self.back[i] = self.left[i]
-            self.left[i] = self.front[i]
-            self.front[i] = tmp_row
-            self.moveFrontBack(self.down, 'd')
+    def move_up_counter(self):
+        tmp = self.right[0]
+        self.right[0] = self.front[0]
+        self.front[0] = self.left[0]
+        self.left[0] = self.back[0]
+        self.back[0] = tmp
+        self.moveFrontCounter(self.up, 'u')
 
-    def move_right(self, counter_clockwise):
+    def move_down(self):
         index = self.size - 1
-        if not counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.down[i][index]
-                self.down[i][index] = self.back[index][0]
-                self.back[index - i][0] = self.up[i][index]
-                self.up[i][index] = self.front[i][index]
-                self.front[i][index] = tmp_row
-            self.moveFront(self.right, 'r')
-        elif counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.down[i][index]
-                self.down[i][index] = self.front[i][index]
-                self.front[i][index] = self.up[i][index]
-                self.up[i][index] = self.back[index - i][0]
-                self.back[index - i][0] = tmp_row
-            self.moveFrontCounter(self.right, 'r')
+        tmp = self.right[index]
+        self.right[index] = self.front[index]
+        self.front[index] = self.left[index]
+        self.left[index] = self.back[index]
+        self.back[index] = tmp
+        self.moveFront(self.down, 'd')
 
-    def move_left(self, counter_clockwise):
+    def move_down_counter(self):
         index = self.size - 1
-        if not counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.down[i][0]
-                self.down[i][0] = self.front[i][0]
-                self.front[i][0] = self.up[i][0]
-                self.up[i][0] = self.back[index - i][index]
-                self.back[index - i][index] = tmp_row
-            self.moveFront(self.left, 'l')
-        elif counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.down[i][0]
-                self.down[i][0] = self.back[index - i][index]
-                self.back[index - i][index] = self.up[i][0]
-                self.up[i][0] = self.front[i][0]
-                self.front[i][0] = tmp_row
-            self.moveFrontCounter(self.left, 'l')
+        tmp = self.right[index]
+        self.right[index] = self.back[index]
+        self.back[index] = self.left[index]
+        self.left[index] = self.front[index]
+        self.front[index] = tmp
+        self.moveFrontCounter(self.down, 'd')
 
-    def move_front(self, counter_clockwise):
+    def move_right(self):
         index = self.size - 1
-        if not counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.up[index][index - i]
-                self.up[index][index - i] = self.left[i][index]
-                self.left[i][index] = self.down[0][i]
-                self.down[0][i] = self.right[index - i][0]
-                self.right[index - i][0] = tmp_row
-            self.moveFront(self.front, 'f')
-        elif counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.up[index][i]
-                self.up[index][i] = self.right[i][0]
-                self.right[i][0] = self.down[0][index - i]
-                self.down[0][index - i] = self.left[index - i][index]
-                self.left[index - i][index] = tmp_row
-            self.moveFrontCounter(self.front, 'f')
+        for i in range(self.size):
+            tmp = self.down[i][index]
+            self.down[i][index] = self.back[index - i][0]
+            self.back[index - i][0] = self.up[i][index]
+            self.up[i][index] = self.front[i][index]
+            self.front[i][index] = tmp
+        self.moveFront(self.right, 'r')
 
-    def move_back(self, counter_clockwise):
+    def move_right_counter(self):
         index = self.size - 1
-        if not counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.up[0][i]
-                self.up[0][i] = self.right[i][index]
-                self.right[i][index] = self.down[index][index - i]
-                self.down[index][index - i] = self.left[index][0]
-                self.left[index][0] = tmp_row
-            self.moveFront(self.back, 'b')
-        elif counter_clockwise:
-            for i in range(self.size):
-                tmp_row = self.up[0][index - i]
-                self.up[0][index - i] = self.left[i][0]
-                self.left[i][0] = self.down[index][i]
-                self.down[index][i] = self.right[index - i][index]
-                self.right[index - i][index] = tmp_row
-            self.moveFrontCounter(self.back, 'b')
+        for i in range(self.size):
+            tmp = self.down[i][index]
+            self.down[i][index] = self.front[i][index]
+            self.front[i][index] = self.up[i][index]
+            self.up[i][index] = self.back[index - i][0]
+            self.back[index - i][0] = tmp
+        self.moveFrontCounter(self.right, 'r')
+
+    def move_left(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.down[i][0]
+            self.down[i][0] = self.front[i][0]
+            self.front[i][0] = self.up[i][0]
+            self.up[i][0] = self.back[index - i][index]
+            self.back[index - i][index] = tmp
+        self.moveFront(self.left, 'l')
+
+    def move_left_counter(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.down[i][0]
+            self.down[i][0] = self.back[index - i][index]
+            self.back[index - i][index] = self.up[i][0]
+            self.up[i][0] = self.front[i][0]
+            self.front[i][0] = tmp
+        self.moveFrontCounter(self.left, 'l')
+
+    def move_front(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.up[index][index - i]
+            self.up[index][index - i] = self.left[i][index]
+            self.left[i][index] = self.down[0][i]
+            self.down[0][i] = self.right[index - i][0]
+            self.right[index - i][0] = tmp
+        self.moveFront(self.front, 'f')
+
+    def move_front_counter(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.up[index][i]
+            self.up[index][i] = self.right[i][0]
+            self.right[i][0] = self.down[0][index - i]
+            self.down[0][index - i] = self.left[index - i][index]
+            self.left[index - i][index] = tmp
+        self.moveFrontCounter(self.front, 'f')
+
+    def move_back(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.up[0][i]
+            self.up[0][i] = self.right[i][index]
+            self.right[i][index] = self.down[index][index - i]
+            self.down[index][index - i] = self.left[index - i][0]
+            self.left[index - i][0] = tmp
+        self.moveFront(self.back, 'b')
+
+    def move_back_counter(self):
+        index = self.size - 1
+        for i in range(self.size):
+            tmp = self.up[0][index - i]
+            self.up[0][index - i] = self.left[i][0]
+            self.left[i][0] = self.down[index][i]
+            self.down[index][i] = self.right[index - i][index]
+            self.right[index - i][index] = tmp
+        self.moveFrontCounter(self.back, 'b')
 
     def moveFront(self, face, l):
         nlist = [[x[i] for x in face] for i in range(len(face[0]))]
@@ -174,28 +181,28 @@ class Cube():
             self.moveFront(self.back, l)
 
     def move_f2(self):
-        self.move_front(False)
-        self.move_front(False)
+        self.move_front()
+        self.move_front()
 
     def move_b2(self):
-        self.move_back(False)
-        self.move_back(False)
+        self.move_back()
+        self.move_back()
 
     def move_d2(self):
-        self.move_down(False)
-        self.move_down(False)
+        self.move_down()
+        self.move_down()
 
     def move_r2(self):
-        self.move_right(False)
-        self.move_right(False)
+        self.move_right()
+        self.move_right()
 
     def move_l2(self):
-        self.move_left(False)
-        self.move_left(False)
+        self.move_left()
+        self.move_left()
 
     def move_u2(self):
-        self.move_up(False)
-        self.move_up(False)
+        self.move_up()
+        self.move_up()
 
     @staticmethod
     def get_color(color):
