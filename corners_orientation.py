ca98f7c041ec829b3f7bae4757b87f987524b93b
Kevin <kemartin@student.42.fr>
2021-01-18 16:01:48 +0100

    v2 branch init

--- corners_orientation.py
+++
@@ -1,9 +0,0 @@
-import cube as c
-import move as m
-import visual as visu
-import utils
-import hta
-
-def main_corner_orientation(cube, pos, lst_move):
-    return cube, lst_move
-
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import numpy as np
-import utils
-import cube as c
-import move
-import hta
-import visual as visu
-import copy
+from SecondaryFunctions import utils, mix
+from Algorithm import Algo as a
+import sys
+import cubik as c
 
 if __name__ == "__main__":
-	options = utils.arg_parse_options()
-	cube = c.Cube(options.mix)
-	cube_before = copy.deepcopy(cube)
-	cube = utils.shuffle_cube(options.mix, cube)
-	cube, lst_moves = hta.main_algo(cube)
-	#print("lst_moves for bad edges orientation : ", lst_moves, "\nnb of move for orientation steps : ", len(lst_moves))
-	if options.visu:
-		visu.main_visual(cube_before, options.mix, lst_moves)
+    args = utils.arg_parse()
+    cube = c.Cube(3)
+    mix_ = mix.Mix()
+    cube.print_cube()
+    if len(args.mix) == 0:
+        print("Random moves generator not triggered, and hand written mix not valid, please try again with a valid mix.")
+        sys.exit(0)
+    lst_moves = args.mix
+    lst_moves = lst_moves.split(' ')
+    cube = mix_.runMix(lst_moves, cube)
+    algo = a.Algo(cube)
+    solution = algo.run()
