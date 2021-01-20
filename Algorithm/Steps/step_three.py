c1532c64151346a12cec5dd5eb447ece2b75b062
arthur laine <arthur.laine99@gmail.com>
2021-01-20 14:16:39 +0100

    step three properly working

--- Algorithm/Steps/step_three.py
+++ Algorithm/Steps/step_three.py
@@ -21,19 +21,19 @@ class step_three:
     def run(self, cubeCurrent, lst_moves):
         self.set_lst_moves(lst_moves)
         if self.finished_three_color_pos(cubeCurrent, ["green", "orange"]) is False:
-                self.moving(cubeCurrent, lst_moves, ["green", "orange"], "front")
+                self.moving(cubeCurrent, ["green", "orange"], "front")
         if self.finished_three_color_pos(cubeCurrent, ["orange", "blue"]) is False:
-                self.moving(cubeCurrent, lst_moves, ["orange", "blue"], "left")
+                self.moving(cubeCurrent, ["orange", "blue"], "left")
         if self.finished_three_color_pos(cubeCurrent, ["blue", "red"]) is False:
-                self.moving(cubeCurrent, lst_moves, ["blue", "red"], "back")
+                self.moving(cubeCurrent, ["blue", "red"], "back")
         if self.finished_three_color_pos(cubeCurrent, ["red", "green"]) is False:
-                self.moving(cubeCurrent, lst_moves, ["red", "green"], "right")
+                self.moving(cubeCurrent, ["red", "green"], "right")
 
     def finished_three_color_pos(self, cubeCurrent, lst_colors):
         return cubik.check_pos_color(self.cubeOrigin, cubeCurrent, lst_colors[0], lst_colors[1])
 
     def update_pos_lst(self, cub, lst_colors):
-        return self.checkerManager.two(cub, lst_colors[0], lst_colors[1])
+        return self.checker.two(cub, lst_colors[0], lst_colors[1])
 
     def moving(self, cubeCurrent, lst_colors, face):
         self.lst_pos_origin = self.update_pos_lst(self.cubeOrigin, lst_colors)
@@ -122,33 +122,33 @@ class step_three:
         elif check_side_lst[1] == "leftPattern":
             self.left_sequence(cubeCurrent, face)
 
-def left_sequence(self, cubeCurrent, face):
-    mixManager = mix.Mix()
+    def left_sequence(self, cubeCurrent, face):
+        mixManager = mix.Mix()
 
-    if face == "front":
-        mixManager.runMix([ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ])
-    elif face == "left":
-        mixManager.runMix([ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ])
-    elif face == "back":
-        mixManager.runMix([ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ])
-    elif face == "right":
-        mixManager.runMix([ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ])
+        if face == "front":
+            mixManager.runMix([ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ])
+        elif face == "left":
+            mixManager.runMix([ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ])
+        elif face == "back":
+            mixManager.runMix([ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ])
+        elif face == "right":
+            mixManager.runMix([ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ])
 
-def right_sequence(self, cubeCurrent, face):
-    mixManager = mix.Mix()
-    if face == "front":
-        mixManager.runMix([ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ])
-    elif face == "left":
-        mixManager.runMix([ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ])
-    elif face == "right":
-        mixManager.runMix([ "D'", "B'", "D", "B", "D", "R", "D'", "R'"], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "B'", "D", "B", "D", "R", "D'", "R'"])
-    elif face == "back":
-        mixManager.runMix([ "D", "R", "D'", "R'", "D'", "B'", "D", "B" ], cubeCurrent)
-        self.lst_moves = utils.append_list(self.lst_moves, [ "D", "R", "D'", "R'", "D'", "B'", "D", "B" ])
+    def right_sequence(self, cubeCurrent, face):
+        mixManager = mix.Mix()
+        if face == "front":
+            mixManager.runMix([ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ])
+        elif face == "left":
+            mixManager.runMix([ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ])
+        elif face == "right":
+            mixManager.runMix([ "D'", "B'", "D", "B", "D", "R", "D'", "R'"], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D'", "B'", "D", "B", "D", "R", "D'", "R'"])
+        elif face == "back":
+            mixManager.runMix([ "D", "R", "D'", "R'", "D'", "B'", "D", "B" ], cubeCurrent)
+            self.lst_moves = utils.append_list(self.lst_moves, [ "D", "R", "D'", "R'", "D'", "B'", "D", "B" ])
