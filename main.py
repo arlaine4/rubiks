27b97b2f0fd6f807373b6714aadb26e587f15f92
arthur laine <arthur.laine99@gmail.com>
2021-01-21 11:47:20 +0100

    old visual implemented for this v2, still the same issue with colors, need to debug in Cube and get_color_index

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from SecondaryFunctions import utils, mix
 from Algorithm import Algo as a
 import sys
 import cubik as c
+import visual as visu
 
 if __name__ == "__main__":
     args = utils.arg_parse()
@@ -14,5 +15,9 @@ if __name__ == "__main__":
     lst_moves = args.mix
     lst_moves = lst_moves.split(' ')
     cube = mix_.runMix(lst_moves, cube)
+    cube.print_cube()
     algo = a.Algo(cube)
     solution = algo.run()
+    if args.visual:
+        visu.main_visual(args.mix, solution)
+    cube.print_cube()
